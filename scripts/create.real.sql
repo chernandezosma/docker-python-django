647952e9d25c4706ca28226d1fcb4444ce4b690f
Cayetano H. Osma <cayetano.hernandez@velocitech.io>
2024-03-23 19:40:47 +0100

    Apache is up, but pyApp not

--- scripts/create.real.sql
+++ scripts/create.real.sql
@@ -1,6 +1,6 @@
 -- Normal database
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-CREATE DATABASE IF NOT EXISTS `python` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `pydb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `dbuser`@`%` IDENTIFIED BY 'dbuser';
-GRANT ALL PRIVILEGES ON python.* TO `dbuser`@`%`;
+GRANT ALL PRIVILEGES ON pydb.* TO `dbuser`@`%`;
 FLUSH PRIVILEGES;
