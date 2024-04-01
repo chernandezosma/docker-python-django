ae2b5db4621c3a94af3278b78bf98cd77b8af5fd
Cayetano H. Osma <cayetano.hernandez@velocitech.io>
2024-04-01 21:13:50 +0200

    - Use the .env variables in all levels - my.cnf according to the recommended by MySQL - Remove useless files - Fix some typos

--- docker/db/scripts/create.test.sql
+++
@@ -1,6 +0,0 @@
--- Normal database
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-CREATE DATABASE IF NOT EXISTS `pydb_test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `dbuser`@`%` IDENTIFIED BY 'dbuser';
-GRANT ALL PRIVILEGES ON pydb_test.* TO `dbuser`@`%`;
-FLUSH PRIVILEGES;
